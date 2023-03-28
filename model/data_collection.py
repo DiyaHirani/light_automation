bf065b1f11b8844c817fd0f9716e8661393b99eb
hirakdesai <hirak214@gmail.com>
2023-03-28 12:13:37 +0530

    Fix: videocapture device

--- model/data_collection.py
+++ model/data_collection.py
@@ -80,7 +80,7 @@ for action in actions:
 
 # Collecting Data
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 # Set mediapipe model
 with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
     # NEW LOOP
